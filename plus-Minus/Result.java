52bd9461ff6c2aca5798f585173697221e41dd08
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-06-22 11:41:26 -0300

    init commit

---
+++ plus-Minus/Result.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static void plusMinus(List<Integer> arr) {
+    // Write your code here
+        int positive = 0;
+        int negative = 0; 
+        int zero = 0; 
+        
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) > 0) {
+                positive++;
+            } else if (arr.get(i) == 0) {
+                zero++;
+            } else {
+                negative++;
+            }
+        }
+        System.out.println((float) positive / arr.size());
+        System.out.println((float) negative / arr.size());
+        System.out.println((float) zero / arr.size());
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
